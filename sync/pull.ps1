b5e2b69256f584a97fe43007916ca7cb09b34b90
charlie <charlie@maddex.dev>
2024-06-20 11:25:25 +0100

    dumping config

--- sync/pull.ps1
+++ sync/pull.ps1
@@ -2,3 +2,4 @@ Copy-Item "./pwsh/Microsoft.PowerShell_profile.ps1" $PROFILE
 Copy-Item "./starship/starship.toml" "$env:USERPROFILE/.config/starship.toml"
 $term_path = (Get-Item -Path "$env:LocalAppData\Packages\Microsoft.WindowsTerminal_*\LocalState\settings.json").FullName
 Copy-Item "./term/settings.json" $term_path
+Copy-Item "./helix/config.toml" "$env:AppData/helix\config.toml"
--- sync/push.ps1
+++ sync/push.ps1
@@ -1,3 +1,4 @@
 Copy-Item $PROFILE "./pwsh/Microsoft.PowerShell_profile.ps1"
 Copy-Item "$env:USERPROFILE/.config/starship.toml" "./starship/starship.toml"
 Copy-Item "$env:LocalAppData\Packages\Microsoft.WindowsTerminal_*\LocalState\settings.json" "./term/settings.json"
+Copy-Item "$env:AppData/helix\config.toml" "./helix/config.toml"
